42d285e18ace09865755e21ccab82ace3f4f9cea
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-03-23 23:42:02 +0200

    Add the snake game project

---
+++ Snake/12. Csharp-OOP-Snake-Workshop-SimpleSnake-Skeleton_6.0/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        public Wall(int leftX, int topY)
+            : base(leftX, topY)
+        {
+            this.InitializeBorders();
+        }
+
+        private void InitializeBorders()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("-");
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("-");
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.SetCursorPosition(49, i);
+                Console.Write("-");
+            }
+
+            Console.SetCursorPosition(0, 10);
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("-");
+            }
+        }
+    }
+}
---
+++ Snake/12. Csharp-OOP-Snake-Workshop-SimpleSnake-Skeleton_6.0/SimpleSnake/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleSnake
+{
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleWindow.CustomizeConsole();
+
+            //Point point = new Point(10, 10);
+            //point.Draw('@');
+
+
+        }
+    }
+}
