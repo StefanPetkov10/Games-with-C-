77d40b458a015ae685044493389e2a8d56ad94ec
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-03-24 18:40:45 +0200

    Update Snake.cs

--- Snake/12. Csharp-OOP-Snake-Workshop-SimpleSnake-Skeleton_6.0/SimpleSnake/GameObjects/Snake.cs
+++ Snake/12. Csharp-OOP-Snake-Workshop-SimpleSnake-Skeleton_6.0/SimpleSnake/GameObjects/Snake.cs
@@ -5,12 +5,38 @@ namespace SimpleSnake.GameObjects
     public class Snake
     {
         private Queue<Point> snake;
+        private Food[] food;
+        private Wall wall;
 
-        public Snake()
+        private int nextLeftX;
+        private int nextTopY;
+
+        public Snake(Wall wall)
         {
             this.snake = new Queue<Point>();
+            this.food = new Food[3];
 
+            this.GetFoods();
             this.CreateSnake();
+            this.wall = wall;
+        }
+
+        public bool IsPointOfWall(Point snakeHead)
+            => snakeHead.LeftX == 0 || snakeHead.LeftX == this.wall.LeftX || snakeHead.TopY == 0 || snakeHead.TopY == this.wall.TopY;
+
+
+        private void GetNextDirection(Point direction, Point snakeHead)
+        {
+            this.nextLeftX = snakeHead.LeftX + direction.LeftX;
+            this.nextTopY = snakeHead.TopY + direction.TopY;
+
+        }
+
+        private void GetFoods()
+        {
+            this.food[0] = new FoodAsterisk(this.wall);
+            this.food[1] = new FoodDollar(this.wall);
+            this.food[2] = new FoodHash(this.wall);
         }
 
         private void CreateSnake()
